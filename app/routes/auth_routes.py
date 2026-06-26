23df669a32a7ec3694932c3b646098af529bcf43
logika <logikalogeswaran@gamil.com>
2026-06-26 09:45:55 +0530

    app routes init fail code writting

---
+++ app/routes/auth_routes.py
@@ -0,0 +1,9 @@
+from flask import Blueprint
+
+from app.controllers import auth_controller as ctrl
+
+auth_bp = Blueprint("auth", __name__, url_prefix="/api/auth")
+
+@auth_bp.route("/register", methods=["POST"])
+def register():
+    return ctrl.register()
